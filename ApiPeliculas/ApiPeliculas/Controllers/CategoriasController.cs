e44986f32e6e4b5969a0d1c3c158bdae2c040349
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-07-25 18:41:10 +0200

    Implementación de ApiController CategoriasController

---
+++ ApiPeliculas/ApiPeliculas/Controllers/CategoriasController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPeliculas.Controllers
+{
+    [ApiController]
+    //[Route("api/[controller]")]  //Esta es una opción común
+    [Route("api/Categorias")]      //Esta opción es mejor si en un futuro cambiamos el nombre de la clase
+
+    public class CategoriasController : ControllerBase
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
