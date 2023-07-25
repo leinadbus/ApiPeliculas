796299b36f5093b4d190cb1946f1a5c281ddbad3
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-07-25 12:46:18 +0200

    Añadida Interfaz ICategoriaRepository

---
+++ ApiPeliculas/ApiPeliculas/Repository/IRepository/ICategoriaRepository.cs
@@ -0,0 +1,19 @@
+﻿using ApiPeliculas.Models;
+
+namespace ApiPeliculas.Repository.IRepository
+{
+    public interface ICategoriaRepository
+    {
+        ICollection<Categoria> GetCategorias();
+
+        Categoria GetCategoria(int CategoriaId);
+        bool ExisteCategoria(string nombreCategoria);
+        bool ExisteCategoria(int CategoriaId);
+        bool CrearCategoria(Categoria categoria);
+        bool ActualizarCategoria(Categoria categoria);
+        bool BorrarCategoria(Categoria categoria);
+        bool Guardar();
+
+
+    }
+}
