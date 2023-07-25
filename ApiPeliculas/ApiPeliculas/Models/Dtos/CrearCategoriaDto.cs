9868812ece45a62c078cb66d2795efed468d6d0e
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-07-25 13:20:01 +0200

    Implementación de CategoriaDto's

---
+++ ApiPeliculas/ApiPeliculas/Models/Dtos/CrearCategoriaDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiPeliculas.Models.Dtos
+{
+    public class CategoriaDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MaxLength(100, ErrorMessage ="El máximo de caracterer es de 100!")]
+        public string Nombre { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+    }
+}
