0a0bbf322e4518b83c41e83b709aac04b7af35c7
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-07-26 18:08:42 +0200

    Implementación Interfaz IUsuarioRepository

---
+++ ApiPeliculas/ApiPeliculas/Repository/IRepository/IUsuarioRepository.cs
@@ -0,0 +1,19 @@
+﻿using ApiPeliculas.Models;
+
+namespace ApiPeliculas.Repository.IRepository
+{
+    public interface IUsuarioRepository
+    {
+        ICollection<Usuario> GetUsuarios();
+
+        Categoria GetCategoria(int CategoriaId);
+        bool ExisteCategoria(string nombreCategoria);
+        bool ExisteCategoria(int CategoriaId);
+        bool CrearCategoria(Categoria categoria);
+        bool ActualizarCategoria(Categoria categoria);
+        bool BorrarCategoria(Categoria categoria);
+        bool Guardar();
+
+
+    }
+}
