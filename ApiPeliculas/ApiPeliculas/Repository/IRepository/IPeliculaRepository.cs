7a7d3eeb1e74bdf9cc9fc8bcb54b1121f9b70289
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-07-26 13:38:14 +0200

    Implementación Interfaz IPeliculaRepository y Repositorio PeliculaRepository

---
+++ ApiPeliculas/ApiPeliculas/Repository/IRepository/IPeliculaRepository.cs
@@ -0,0 +1,25 @@
+﻿using ApiPeliculas.Models;
+
+namespace ApiPeliculas.Repository.IRepository
+{
+    public interface IPeliculaRepository
+    {
+        ICollection<Pelicula> GetPeliculas();
+
+        Pelicula GetPelicula(int PeliculaId);
+        bool ExistePelicula(string nombrePelicula);
+        bool ExistePelicula(int PeliculaId);
+        bool CrearPelicula(Pelicula pelicula);
+        bool ActualizarPelicula(Pelicula pelicula);
+        bool BorrarPelicula(Pelicula pelicula);
+
+        //Métodos de busqueda de peliculas en categorias y busqueda por nombre
+        ICollection<Pelicula> GetPeliculasEnCategoria(int categoriaId);
+        ICollection<Pelicula> BuscarPelicula(string nombre);
+
+
+        bool Guardar();
+
+
+    }
+}
---
+++ ApiPeliculas/ApiPeliculas/Repository/PeliculaRepository.cs
@@ -0,0 +1,77 @@
+﻿using ApiPeliculas.Data;
+using ApiPeliculas.Models;
+using ApiPeliculas.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+
+namespace ApiPeliculas.Repository
+{
+    public class PeliculaRepository : IPeliculaRepository
+    {
+        private readonly AplicationDbContext _bd;
+
+        public PeliculaRepository(AplicationDbContext _bd)
+        {
+            this._bd = _bd;
+        }
+        public bool ActualizarPelicula(Pelicula pelicula)
+        {
+            pelicula.FechaCreacion = DateTime.Now;
+            _bd.Peliculas.Update(pelicula);
+            return Guardar();
+        }
+
+        public bool BorrarPelicula(Pelicula pelicula)
+        {
+            _bd.Peliculas.Remove(pelicula);
+            return Guardar();
+        }
+
+        public ICollection<Pelicula> BuscarPelicula(string nombre)
+        {
+            IQueryable<Pelicula> query = _bd.Peliculas;
+            if (!string.IsNullOrEmpty(nombre))
+            {
+                query = query.Where(e => e.Nombre.Contains(nombre) || e.Descripcion.Contains(nombre);
+            }
+            return query.ToList();
+        }
+
+        public bool CrearPelicula(Pelicula pelicula)
+        {
+            pelicula.FechaCreacion = DateTime.Now;
+            _bd.Peliculas.Add(pelicula);
+            return Guardar();
+        }
+
+        public bool ExistePelicula(string nombrePelicula)
+        {
+            return _bd.Peliculas.Any(category => category.Nombre.ToLower().Trim() == nombrePelicula.ToLower().Trim());
+        }
+
+        public bool ExistePelicula(int peliculaId)
+        {
+            return _bd.Peliculas.Any(pelicula => pelicula.Id == peliculaId);
+        }
+
+        public Pelicula GetPelicula(int peliculaId)
+        {
+           return _bd.Peliculas.FirstOrDefault(pelicula => pelicula.Id == peliculaId);
+        }
+
+        public ICollection<Pelicula> GetPeliculas()
+        {
+            return _bd.Peliculas.OrderBy(pelicula => pelicula.Nombre).ToList();
+        }
+
+        public ICollection<Pelicula> GetPeliculasEnCategoria(int categoriaId)
+        {
+            return _bd.Peliculas.Include(ca => ca.Categoria).Where(ca => ca.categoriaId == categoriaId).ToList();
+        }
+
+        public bool Guardar()
+        {
+            return _bd.SaveChanges() >= 0 ? true : false;
+        }
+    }
+}
